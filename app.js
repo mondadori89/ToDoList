4b8bac000b89570912f0c5491616f11d89eafe69
Monda <lmondadori@gmail.com>
2022-01-20 22:56:23 -0300

    to do list v.1.1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// ---- To do list v1.0 ---- com remove button funcionando \o/
+// ---- To do list v1.1 ---- com remove button funcionando \o/
 //próximo passo: adicionar botão 'done'
 
 // nomeando acionadores do html
@@ -11,18 +11,39 @@ function addTask() {
     let task = document.getElementById('task').value
     if (task) {
         let listItem = document.createElement('li');
+        let done = document.createElement('span');
         let remove = document.createElement('span');
         let espaco = document.createElement('br');
 
+        done.id = 'done';
         remove.id = 'remove';
         listItem.id = 'listItem';
         espaco.id = 'espaco';
 
         listItem.innerHTML = task;
 
+        listItem.appendChild(done);
         listItem.appendChild(remove);
         document.getElementById('olist').appendChild(listItem);
         document.getElementById('olist').appendChild(espaco);
+
+        // Botão de check e unchuck da task
+        let clicked = '';
+        done.onclick = function() {
+            if (clicked) {
+                done.style.backgroundImage = "url('images/task_icon.png')";
+                done.style.backgroundSize = "17px";
+                done.style.marginTop = "1px";
+                done.style.marginLeft = "20px";
+                clicked = false;
+            } else {
+                done.style.backgroundImage = "url('images/check_icon.png')";
+                done.style.backgroundSize = "20px";
+                done.style.marginTop = "0px";
+                done.style.marginLeft = "19px";
+                clicked = true;
+            }
+        }
 
         remove.onclick = function() {
             olist.removeChild(listItem);
