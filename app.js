65c5a5f9799272c31f3d464363bd7d452cac2fce
Monda <lmondadori@gmail.com>
2022-01-27 13:44:40 -0300

    v1.3

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// ---- To do list v1.2 ---- com remove e done button BIRLL
-//Próximo Passo: arrumar style?
+// ---- To do list v1.3 ---- Style melhorado
+//Próximo Passo: não sei ainda
 
 // nomeando acionadores do html
 let submitButton = document.getElementById('submit');
@@ -10,44 +10,49 @@ let orderedList = document.getElementById('olist');
 function addTask() {
     let task = document.getElementById('task').value
     if (task) {
-        let listItem = document.createElement('li');
+        let listConteiner = document.createElement('li');
+        let listItem = document.createElement('p');
         let doneButton = document.createElement('span');
         let removeButton = document.createElement('span');
-        let breakSpace = document.createElement('br');
+        //let breakSpace = document.createElement('br');
 
-        doneButton.id = 'done';
-        removeButton.id = 'remove';
+        listConteiner.id = 'listConteiner';
         listItem.id = 'listItem';
-        breakSpace.id = 'espaco';
+        doneButton.id = 'done';
+        removeButton.id = 'remove';   
+        //breakSpace.id = 'espaco';
 
         listItem.innerHTML = task;
 
-        listItem.appendChild(doneButton);
-        listItem.appendChild(removeButton);
-        document.getElementById('olist').appendChild(listItem);
-        document.getElementById('olist').appendChild(breakSpace);
+        listConteiner.appendChild(listItem);
+        listConteiner.appendChild(doneButton);
+        listConteiner.appendChild(removeButton);
+        document.getElementById('olist').appendChild(listConteiner);
+        //document.getElementById('olist').appendChild(breakSpace);
 
-        // Botão de check e unchuck da task
+        // Botão de check e uncheck da task
         let isClicked = '';
         doneButton.onclick = function() {
             if (isClicked) {
                 doneButton.style.backgroundImage = "url('images/task_icon.png')";
                 doneButton.style.backgroundSize = "17px";
-                doneButton.style.marginTop = "1px";
+                doneButton.style.height = "19px";
+                doneButton.style.width = "19px";
                 doneButton.style.marginLeft = "20px";
                 isClicked = false;
             } else {
                 doneButton.style.backgroundImage = "url('images/check_icon.png')";
                 doneButton.style.backgroundSize = "20px";
-                doneButton.style.marginTop = "0px";
+                doneButton.style.height = "20px";
+                doneButton.style.width = "20px";
                 doneButton.style.marginLeft = "19px";
                 isClicked = true;
             }
         }
 
         removeButton.onclick = function() {
-            orderedList.removeChild(listItem);
-            orderedList.removeChild(breakSpace);
+            orderedList.removeChild(listConteiner);
+            //orderedList.removeChild(breakSpace);
         }
     } else {alert('No item to add')}
 }
